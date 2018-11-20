dedaa3dad13fb2f60cf418605e57fc2048d5549e
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-11-20 13:19:50 -0500

    Add more images to cache by service worker

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,16 @@ var cacheFiles = [
 	'./js/restaurant_info.js',
 	'./js/main.js',
 	'./img/1.jpg',
-	'./img/2.jpg'
+	'./img/2.jpg',
+	'./img/3.jpg',
+	'./img/4.jpg',
+	'./img/5.jpg',
+	'./img/5.jpg',
+	'./img/6.jpg',
+	'./img/7.jpg',
+	'./img/8.jpg',
+	'./img/9.jpg',
+	'./img/10.jpg'
 ];
 
 
