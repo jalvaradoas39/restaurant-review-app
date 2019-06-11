e281182a703d2df94e831771686d305681466703
Jorge Alvarado <jalvaradoas39@outlook.com>
2019-06-11 15:51:50 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,20 +6,6 @@ var markers = []
 
 
 
-/**
- * Register Service Worker
- */
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.
-    register('./service-worker.js', {scope: './'})
-    .then(function(registration) {
-      console.log('[ServiceWorker] registration succeeded');
-    }).catch(function(err) {
-      console.log('Service worker registration failed');
-    });
-}
-
-
 
 
 /**
